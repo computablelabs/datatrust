699f2a8d1cc4462797d9afaa8bed566b17f0bc4b
Will Button <will@willbutton.co>
2019-05-13 15:38:15 -0700

    Obfuscate results returned by listing GET endpoint

--- Makefile
+++ Makefile
@@ -16,6 +16,6 @@ docker-build:
 
 docker-deploy:
 	docker build -t datatrust:local .
-	eval $(aws ecr get-login --no-include-email --region us-west-1 --profile computable)
+	$(shell aws ecr get-login --no-include-email --region us-west-1 --profile computable)
 	docker tag datatrust:local 365035671514.dkr.ecr.us-west-1.amazonaws.com/computable/datatrust-api:latest
 	docker push 365035671514.dkr.ecr.us-west-1.amazonaws.com/computable/datatrust-api:latest
--- api/v1/listing.py
+++ api/v1/listing.py
@@ -2,6 +2,7 @@
 Resource definition for Drive Listing
 """
 
+from datetime import datetime
 from flask import request
 from flask_restful import Resource
 from flask_restful_swagger import swagger
@@ -47,7 +48,9 @@ class Listing(Resource):
         if response == 'Error retrieving record':
             return {'message': 'Error retrieving record'}, 500
         else:
-            return response, 200
+            print(response)
+            obfuscated_response = self.obfuscate_listing(response)
+            return obfuscated_response, 200
 
     @swagger.operation(
         notes='Add a new listing',
@@ -137,3 +140,67 @@ class Listing(Resource):
             return {'message': response }, 500
         else:
             return {'message': response}, 200
+
+    def elapsed_time(self, start, end):
+        """
+        Return a reasonable number for elapsed time
+        :param start: datetime
+        :param end: datetime
+        :return: elapsed: string
+        """
+        duration = end - start
+        if duration.days == 0:
+            if duration.seconds > 3600:
+                return f'~{duration.seconds // 3600} hours'
+            elif duration.seconds > 60:
+                return f'~{duration.seconds // 60} minutes'
+            else:
+                return f'~{duration.seconds} seconds'
+        else:
+            return f'{duration.days} days'
+
+    def obfuscate_listing(self, listing):
+        """
+        Obfuscate the data returned from the db before presenting to the client
+        Fields returned:
+        listing: string: hash of the listing (a.k.a. id)
+        num_data_points: int: number of data points in the listing
+        avg_velocity: float: average velocity across all data points
+        duration: string: elapsed time between first and last data point
+        start_location: string: City, State location of the first data point
+        end_location: string: City, State location of the last data point
+        :param listing: dict: a Drive listing
+        :return: obfuscated: dict: an obfuscated listing
+        """
+        obfuscated = {}
+        obfuscated['listing'] = listing['listing']
+        obfuscated['num_data_points'] = len(listing['data'])
+        sum_velocity = 0
+        velocity_points = 0
+        start_timestamp = datetime.now()
+        end_timestamp = datetime(1970, 1, 1, 12, 0, 0)
+        start_location = None
+        end_location = None
+        for location in listing['data']:
+            if 'velocity' in location:
+                sum_velocity = sum_velocity + float(location['velocity'])
+                velocity_points = velocity_points + 1
+            if 'adjusted_timestamp' in location:
+                ts = datetime.strptime(location['adjusted_timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+                if ts < start_timestamp:
+                    start_timestamp = ts
+                    if 'latitude' and 'longitude' in location:
+                        lat = round(float(location['latitude']), 2)
+                        long = round(float(location['longitude']), 2)
+                        start_location = f"{lat}, {long}"
+                if ts > end_timestamp:
+                    end_timestamp = ts
+                    if 'latitude' and 'longitude' in location:
+                        lat = round(float(location['latitude']), 2)
+                        long = round(float(location['longitude']), 2)
+                        end_location = f"{lat}, {long}"
+        obfuscated['avg_velocity'] = sum_velocity / velocity_points
+        obfuscated['duration'] = self.elapsed_time(start_timestamp, end_timestamp)
+        obfuscated['start_location'] = start_location
+        obfuscated['end_location'] = end_location
+        return obfuscated
